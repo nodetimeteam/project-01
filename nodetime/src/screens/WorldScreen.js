3036ee1a56ec9ed9cda78f2c82daf6fdefa3477f
Mitch Aguinaldo <mitchellaguinaldo@gmail.com>
2018-03-31 09:39:10 -0700

    Added A, B, 0-3 and a function to test

--- nodetime/src/screens/WorldScreen.js
+++ nodetime/src/screens/WorldScreen.js
@@ -13,10 +13,18 @@ class WorldScreen extends React.Component {
   };
 
   toggleFunction = () => {
-    (this.state.currentLetter === "A")
-      ? this.setState({currentLetter: "B"})
-      : this.setState({currentLetter: "A"});
-    ; 
+    let list = [
+      "A", "B",
+      //  "C", "D", "E",
+      // "F", "G", "H", "I", "J",
+      // "K", "L", "M", "N", "O",
+      // "P", "Q", "R", "S", "T",
+      // "U", "V", "W", "X", "Y",
+      // "Z", 
+      0, 1, 2, 3//, 4, 5, 6, 7, 8, 9
+    ];
+    let nextIndex = list.indexOf(this.state.currentLetter) + 1
+    this.setState({currentLetter: list[nextIndex]}); 
   };
 
   render() {
--- nodetime/src/services/asl-svg.services.js
+++ nodetime/src/services/asl-svg.services.js
@@ -5,6 +5,10 @@
 const ASL = {
     A: require('../assets/ASL/ASL_A.png'),
     B: require('../assets/ASL/ASL_B.png'),
+    0: require('../assets/ASL/ASL_0_Num.png'),
+    1: require('../assets/ASL/ASL_1_Num.png'),
+    2: require('../assets/ASL/ASL_2_Num.png'),
+    3: require('../assets/ASL/ASL_3_Num.png'),
 };
 
 export default ASL;
