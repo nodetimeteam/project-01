f3b3ff9b9572977fb54665844bdfbfc6930a63bb
eeeeeeekjgfaaa <eeeeeeekjgfaaa@asdfmcbp.local>
2018-03-31 15:34:23 -0700

    working

--- nodetime/App.js
+++ nodetime/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import LoginScreen from './src/screens/LoginScreen';
 import GameScreen from './src/screens/GameScreen';
+import {AudioRecorder, AudioUtils} from 'react-native-audio';
+let audioPath = AudioUtils.DocumentDirectoryPath + '/test.aac';
 
+AudioRecorder.prepareRecordingAtPath(audioPath, {
+  SampleRate: 22050,
+  Channels: 1,
+  AudioQuality: "Low",
+  AudioEncoding: "amr_wb"
+});
 
 export default class App extends React.PureComponent {
   render() {
---
+++ nodetime/src/services/googleSpeech.js
@@ -0,0 +1,39 @@
+
+
+
+import axios from 'axios';
+import GOOGLE_SPEECH from '../../config/googleSpeech'
+
+const googleSpeech = {
+    create: () => {
+        let data = {
+            "config": {
+                "encoding": "FLAC",
+                "sampleRateHertz": 16000,
+                "languageCode": "en-US",
+                "enableWordTimeOffsets": false
+            },
+            "audio": {
+                "uri": "gs://cloud-samples-tests/speech/brooklyn.flac"
+            }
+        }
+        const config = {
+            method: 'POST',
+            headers,
+            data: data
+        }
+
+        return axios(`${GOOGLE_SPEECH}`, config)
+            .then(responseSuccessHandler)
+            .catch(responseErrorHandler)
+    },
+
+}
+
+const responseSuccessHandler = response => response.data;
+const responseErrorHandler = error => {
+    console.log(error);
+    return Promise.reject(error);
+}
+
+export default googleSpeech
