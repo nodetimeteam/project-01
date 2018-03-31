2c6353b8d1d1d2768aac63bbc14c4dd18837f1fd
eeeeeeekjgfaaa <eeeeeeekjgfaaa@asdfmcbp.local>
2018-03-31 09:08:37 -0700

    workging

--- nodetime/android/app/src/main/java/com/nodetime/MainApplication.java
+++ nodetime/android/app/src/main/java/com/nodetime/MainApplication.java
@@ -3,7 +3,6 @@ package com.nodetime;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.wenkesj.voice.VoicePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VoicePackage()
+          new MainReactPackage()
       );
     }
 
