a77fbd0f7809d00189667afad34ab72b8a75df08
David Carranza <david.carranza.ca@gmail.com>
2018-03-31 04:22:00 -0700

    Login Form done

--- nodetime/src/screens/LoginScreen.js
+++ nodetime/src/screens/LoginScreen.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Card, Icon, FormLabel } from 'react-native-elements';
+import FormInputs from '../components/form-inputs'
 
 
 class LoginScreen extends React.PureComponent {
     static navigationOptions = {
         header: null
       };
+      constructor(props) {
+          super(props);
+        this.state = {
+            userName: '',
+            password: ''
+        }
+      }
 
     onLogin = e => {
-        // this does stuff either in node or it will login someone
+        
     }
 
     render() {
         return (
             <View style={styles.container}>
+            <Card
+              title="Login"
+              tityleStyle={{fontSize: '300'}}
+              containerStyle={{width: '95%', borderRadius: 10}}
+            >
+            <View>
+                <FormLabel>User Name</FormLabel>
+                <FormInputs
+                    onChangeText={userName => this.setState({userName})} 
+                    value={this.state.userName}
+                />
+
+                <FormLabel>Password</FormLabel>
+                <FormInputs 
+                    onChangeText={password => this.setState({password})}
+                    value={this.state.password}
+                    secureTextEntry={true}
+                />
+                
+            </View>
                 <TouchableOpacity 
                     style={styles.btn}
                     onPress={this.onLogin}
@@ -21,7 +50,8 @@ class LoginScreen extends React.PureComponent {
                     <Text style={styles.text}>
                         Login
                     </Text>
-                </TouchableOpacity>                
+                </TouchableOpacity>
+                </Card>                
             </View>
         );
     }
@@ -39,9 +69,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#000',
         borderRadius: 50,
-        width: 300,
         paddingTop: 20,
-        paddingBottom: 20
+        paddingBottom: 20,
+        marginTop: 20,
+        marginBottom: 20
     },
     text: {
         textAlign: 'center',
