b92e0ecfed4151418291ce1f59e926f75212a16b
Mitch Aguinaldo <mitchellaguinaldo@gmail.com>
2018-03-31 10:35:42 -0700

    Added F-J

--- nodetime/src/screens/GameScreen.js
+++ nodetime/src/screens/GameScreen.js
@@ -15,7 +15,7 @@ class GameScreen extends React.Component {
   toggleFunction = () => {
     let list = [
       "A", "B", "C", "D", "E",
-      // "F", "G", "H", "I", "J",
+      "F", "G", "H", "I", "J",
       // "K", "L", "M", "N", "O",
       // "P", "Q", "R", "S", "T",
       // "U", "V", "W", "X", "Y",
--- nodetime/src/services/asl-svg.services.js
+++ nodetime/src/services/asl-svg.services.js
@@ -8,6 +8,14 @@ const ASL = {
   C: require('../assets/ASL/ASL_C.png'),
   D: require('../assets/ASL/ASL_D.png'),
   E: require('../assets/ASL/ASL_E.png'),
+
+  F: require('../assets/ASL/ASL_F.png'),
+  G: require('../assets/ASL/ASL_G.png'),
+  H: require('../assets/ASL/ASL_H.png'),
+  I: require('../assets/ASL/ASL_I.png'),
+  J: require('../assets/ASL/ASL_J.png'),
+  K: require('../assets/ASL/ASL_K.png'),
+
   0: require('../assets/ASL/ASL_0_Num.png'),
   1: require('../assets/ASL/ASL_1_Num.png'),
   2: require('../assets/ASL/ASL_2_Num.png'),
